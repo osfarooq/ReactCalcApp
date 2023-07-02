9b21019d2d91b6b472c87c8cb837a7ec8ccb551b
osfarooq <osama.farooq1308@gmail.com>
2023-07-02 23:56:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result + e.target.name);
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const clear = () => {
+    setResult("");
+  };
+
+  const handleOperatorClick = (operator) => {
+    setResult(result + operator);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <div className="topBox">
+          <a className="casio">C A S I O</a>
+          <div className="sensor">| | | | | | | | |</div>
+        </div>
+        <input className="inputBox" type="text" value={result} />
+        <div className="keypads">
+          <div className="keypad1">
+            <button name="1" onClick={handleClick}>
+              1
+            </button>
+            <button name="2" onClick={handleClick}>
+              2
+            </button>
+            <button name="3" onClick={handleClick}>
+              3
+            </button>
+            <button name="4" onClick={handleClick}>
+              4
+            </button>
+            <button name="5" onClick={handleClick}>
+              5
+            </button>
+            <button name="6" onClick={handleClick}>
+              6
+            </button>
+            <button name="7" onClick={handleClick}>
+              7
+            </button>
+            <button name="8" onClick={handleClick}>
+              8
+            </button>
+            <button name="9" onClick={handleClick}>
+              9
+            </button>
+            <button name="0" onClick={handleClick}>
+              0
+            </button>
+            {/* ... other buttons */}
+            <button className="operator" name="=" onClick={calculate}>
+              =
+            </button>
+            <button className="clear" onClick={clear}>
+              C
+            </button>
+          </div>
+          <div className="keypad2">
+            <button className="add" onClick={() => handleOperatorClick("+")}>
+              +
+            </button>
+            <button
+              className="subtract"
+              onClick={() => handleOperatorClick("-")}
+            >
+              -
+            </button>
+            <button
+              className="multiply"
+              onClick={() => handleOperatorClick("*")}
+            >
+              *
+            </button>
+            <button className="divide" onClick={() => handleOperatorClick("/")}>
+              /
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
